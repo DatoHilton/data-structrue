0bda05f5f7cf41afed6a7a36017f79ad9b54f66c
洪图 <fs50ht@tju.edu.cn>
2023-01-21 01:49:14 +0800

    finish sequence list

--- linear list/sequenceList.cpp
+++ linear list/sequenceList.cpp
@@ -2,7 +2,7 @@
 #include "define.h"
 
 // 顺序表的存储结构
-#define MAXIZE 100
+#define MAXSIZE 100
 typedef int ElemType;
 typedef struct __SqList {
     ElemType* elem;
@@ -11,7 +11,7 @@ typedef struct __SqList {
 
 // 顺序表的初始化
 Status InitList(SqList& L) {
-    L.elem = new ElemType[MAXIZE];
+    L.elem = new ElemType[MAXSIZE];
     if (!L.elem)
         exit(OVERFLOW);
     L.length = 0;
@@ -47,5 +47,37 @@ Status GetElem(SqList L, int i, ElemType& e) {
     if (i < 1 || i > L.length)
         return ERROR;
     e = L.elem[i - 1];
+    return OK;
+}
+
+// 顺序表的查找
+int LocateElem(SqList L, ElemType e) {
+    for (int i = 0; i < L.length; i++) {
+        if (L.elem[i] == e)
+            return i + 1;
+    }
+    return 0;
+}
+
+// 顺序表的插入
+Status ListInsert(SqList& L, int i, ElemType e) {
+    if ((i < 1) || (i > L.length + 1))
+        return ERROR;
+    if (L.length == MAXSIZE)
+        return ERROR;
+    for (int j = L.length - 1; j >= i - 1; j--)
+        L.elem[j + 1] = L.elem[j];
+    L.elem[i - 1] = e;
+    L.length++;
+    return OK;
+}
+
+// 顺序表的删除
+Status ListDelete(SqList& L, int i) {
+    if ((i < 1) || (i > L.length))
+        return ERROR;
+    for (int j = i; j <= L.length - 1; j++)
+        L.elem[j - 1] = L.elem[j];
+    L.length--;
     return OK;
 }
